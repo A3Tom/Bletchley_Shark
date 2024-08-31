edd52aa2f61866b280248ca1a37d3b3be969c789
Tom Smith <Tom_Esq@Yahoo.com>
2024-08-31 15:49:02 +0100

    Removed misspelled folder

--- encrpytion/dotnet/BletchleyShark.Encryption.Application/Strategy/KeyGeneration/DiffieHellmanKeyGenerationStrategy.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Numerics;
-
-namespace BletchleyShark.Encryption.Application.Strategy.KeyGeneration;
-public static class DiffieHellmanKeyGenerationStrategy
-{
-    public static BigInteger GeneratePublicTransportValue(BigInteger publicBase, BigInteger partySecret, BigInteger publicModulus) => BigInteger.ModPow(publicBase, partySecret, publicModulus);
-
-    public static BigInteger GenerateCommonSecretKey(BigInteger incomingTransportValue, BigInteger partySecret, BigInteger publicModulus) => BigInteger.ModPow(incomingTransportValue, partySecret, publicModulus);
-}
--- encrpytion/dotnet/BletchlyShark.Encryption/Controllers/Abstract/BaseController.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace BletchleyShark.Encryption.API.Controllers.Abstract;
-
-[Route("api/[controller]")]
-public class BaseController : Controller
-{
-}
